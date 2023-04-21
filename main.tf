713a737cdd7c9d9abf748920246fcf527579fec9
Edgars Cirulis <edgars@precisionlex.com>
2023-04-21 22:30:23 +0300

    Update Ansible tasks and Terraform script

--- main.tf
+++ main.tf
@@ -24,6 +24,13 @@ resource "aws_security_group" "final_assignment_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
